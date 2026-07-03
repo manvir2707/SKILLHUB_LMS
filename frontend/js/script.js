0bb090d9087354ab43c1a6a1711e81eccbd07c95
manvir2707 <1man.vir27@gmail.com>
2026-07-03 18:22:02 +0530

    updated

---
+++ frontend/js/script.js
@@ -0,0 +1,28 @@
+console.log("Welcome to SkillHub LMS");
+let trainer = "Manish Rai";
+console.log(trainer);
+
+let course = "MERN";
+let duration = 45;
+let fees = 12000;
+console.log(course);
+console.log(duration);
+console.log(fees);
+
+// Data Types example
+let student = "Rahul";
+// String
+let age = 20;
+// Number
+let enrolled = true;
+// Boolean
+console.log(student);
+console.log(age);
+console.log(enrolled);
+
+let student = "Manvir Kaur";
+let age = 19;
+let enrolled = true;
+console.log(student);
+console.log(age);
+console.log(enrolled);
